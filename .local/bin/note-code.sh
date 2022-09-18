52e075dff8797851b5edde4352c8909c523c3ea8
kawsarahmed-012 <mdkowsir369@gmail.com>
2022-09-19 04:06:30 +0600

    structural change in note directory

--- .local/bin/note-code.sh
+++ .local/bin/note-code.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/dash
 
-B_path="${1%/}"
-img="codeshot_$(date +%Y-%m-%d_%H:%M:%S).jpg"
+b_path="${1%/}"
+t_name="$2"
+t_index="$(echo $t_name | sed 's| .*||')"
+img="${t_index}_codeshot_$(date +%Y-%m-%d_%H:%M:%S).jpg"
 scrot "$img" -z -a $(slop -f '%x,%y,%w,%h')
 [ ! -f "$img" ] && notify-send "No screenshot captured." && exit 0
 code="$(tesseract --psm 6 $img - | sed 's|^|        |')"
-mv "$img" "$B_path/images/"
-echo "\n![code shot](./images/$img)\n" >> "$B_path/main.md"
-echo "$code\n" >> "$B_path/main.md"
+mv "$img" "$b_path/images/"
+echo "\n![code shot](./images/$img)" >> "$b_path/$t_name.md"
+echo "$code\n" >> "$b_path/$t_name.md"

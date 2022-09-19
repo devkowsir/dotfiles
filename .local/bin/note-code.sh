732240560fbd841601878ac59d19206cd80168c0
kawsarahmed-012 <mdkowsir369@gmail.com>
2022-09-19 21:19:33 +0600

    Codes can be inserted with backticks as well.

--- .local/bin/note-code.sh
+++ .local/bin/note-code.sh
@@ -6,7 +6,7 @@ t_index="$(echo $t_name | sed 's| .*||')"
 img="${t_index}_codeshot_$(date +%Y-%m-%d_%H:%M:%S).jpg"
 scrot "$img" -z -a $(slop -f '%x,%y,%w,%h')
 [ ! -f "$img" ] && notify-send "No screenshot captured." && exit 0
-code="$(tesseract --psm 6 $img - | sed 's|^|        |')"
+code="$(tesseract --psm 6 $img -)"
 mv "$img" "$b_path/images/"
 echo "\n![code shot](./images/$img)" >> "$b_path/$t_name.md"
 echo "$code\n" >> "$b_path/$t_name.md"
